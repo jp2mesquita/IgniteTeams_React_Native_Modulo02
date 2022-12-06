5771a8e5cc174d5bf2e8025e37a4e27bb9b272fb
João Paulo Mesquita <jp-mesquita@live.com>
2022-12-06 16:52:40 -0300

    feat: create NewGroup screen

---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@components/Button";
+import { Header } from "@components/Header";
+import { HighLight } from "@components/HighLight";
+import { Input } from "@components/Input";
+
+import { Container, Content, Icon } from "./styles";
+
+export function NewGroup(){
+  return(
+    <Container>
+      <Header showBackButton/>
+
+      <Content>
+        <Icon />
+
+        <HighLight 
+          title="Nova turma"
+          subTitle="Crie  a  turma para adicionar as pessoas"
+        />
+
+        <Input 
+          placeholder="Nome da turma"
+        />
+
+        <Button  
+          title="Criar"
+          style={ { marginTop: 16}}
+        />
+        
+      </Content>
+    </Container>
+  )
+}
