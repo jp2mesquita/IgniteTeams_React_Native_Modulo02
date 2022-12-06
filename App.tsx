7ad53d4506a8f033f1e264ce6d87be3f9b30e7d2
João Paulo Mesquita <jp-mesquita@live.com>
2022-12-06 09:44:44 -0300

    feat: seting statusbar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
 import theme from './src/theme'
 
 import { Groups } from '@screens/Groups';
@@ -11,7 +13,12 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      { !fontsLoaded ?  <Groups /> : <Loading />}
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      { fontsLoaded ?  <Groups /> : <Loading />}
        
     </ThemeProvider>
   );
