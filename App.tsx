ff8e247736f6d934272597335b783fd2dc32d175
João Paulo Mesquita <jp-mesquita@live.com>
2022-12-06 09:42:09 -0300

    feat: create load indicator as a component

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
-import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import theme from './src/theme'
 
 import { Groups } from '@screens/Groups';
+import { Loading } from '@components/Loading';
 
 export default function App() {
 
@@ -11,8 +11,8 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ?  <Groups /> : <ActivityIndicator />}
-     
+      { !fontsLoaded ?  <Groups /> : <Loading />}
+       
     </ThemeProvider>
   );
 }
