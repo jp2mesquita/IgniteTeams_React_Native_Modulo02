5d0b5ec04fa3dc071d0426bb0b1c3d6ad8b433cd
João Paulo Mesquita <jp-mesquita@live.com>
2022-12-06 10:41:16 -0300

    feat: create group card component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,7 +1,10 @@
 
 import { Header } from '@components/Header';
 import { HighLight } from '@components/HighLight';
+import { GroupCard } from '@components/GroupCard';
+
 import { Container } from './styles';
+import { Alert } from 'react-native';
 
 export function Groups() {
   return (
@@ -11,6 +14,8 @@ export function Groups() {
         title='Turmas'
         subTitle='Jogue com a sua turma'
       />
+
+      <GroupCard title='Galera do Ignite' onPress={() => Alert.alert("Open Card")}/>
     </Container>
   );
 }
