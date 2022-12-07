3038cbc5c0e832d2dc48e9c43da7dd75c950e62b
João Paulo Mesquita <jp-mesquita@live.com>
2022-12-07 16:52:25 -0300

    feat: create Players screen

---
+++ src/screens/Players/index.tsx
@@ -0,0 +1,29 @@
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Header } from "@components/Header";
+import { HighLight } from "@components/HighLight";
+import { Input } from "@components/Input";
+
+import { Container, Form } from "./styles";
+
+export function Players(){
+  return(
+    <Container>
+      <Header showBackButton/>
+
+      <HighLight 
+        title="Nome da turma"
+        subTitle="Adicione  a galera e separe os times"
+      />
+
+      <Form>
+        <Input 
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
+        <ButtonIcon
+          icon="add" 
+          />
+      </Form>
+    </Container>
+  )
+}
