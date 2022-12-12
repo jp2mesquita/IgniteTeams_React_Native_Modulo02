043263ad34bf64e6e4f825a8ab1a94fdca49a5c6
João Paulo Mesquita <jp-mesquita@live.com>
2022-12-12 12:24:18 -0300

    feat: add navigation through buttons

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,10 +2,18 @@ import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { HighLight } from "@components/HighLight";
 import { Input } from "@components/Input";
+import { useNavigation } from "@react-navigation/native";
 
 import { Container, Content, Icon } from "./styles";
 
 export function NewGroup(){
+
+  const { navigate } = useNavigation()
+
+  function handleNew(){
+    navigate('players', {group: 'Aposentados'})
+  }
+
   return(
     <Container>
       <Header showBackButton/>
@@ -25,6 +33,7 @@ export function NewGroup(){
         <Button  
           title="Criar"
           style={ { marginTop: 16}}
+          onPress={handleNew}
         />
         
       </Content>
