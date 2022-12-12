9d98b1056b2e6288137ac021d9302849243ef653
João Paulo Mesquita <jp-mesquita@live.com>
2022-12-12 11:09:35 -0300

    feat: create  navitagion  types definition

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+  namespace ReactNavigation {
+    interface  RootParamList {
+      groups: undefined
+      new: undefinded
+      players: {
+        group: string
+      }
+    }
+  }
+}
