371c4b5fb1270def56faf2063f4c9ed45deac033
João Paulo Mesquita <jp-mesquita@live.com>
2022-12-12 11:09:14 -0300

    refator: change  the imports order

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,13 +1,14 @@
+import { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { Alert, FlatList } from 'react-native';
 
 import { Header } from '@components/Header';
 import { HighLight } from '@components/HighLight';
 import { GroupCard } from '@components/GroupCard';
-
-import { Container } from './styles';
-import { Alert, FlatList } from 'react-native';
-import { useState } from 'react';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
+
+import { Container } from './styles';
 
 export function Groups() {
   const [ groups, setGroups ] = useState<string[]>([])
@@ -42,6 +43,6 @@ export function Groups() {
         title='Criar nova turma'
       />
     </Container>
-  );
+  ); 
 }
 
